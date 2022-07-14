9f1d753f1b5222cb026a3ae04ad5da4f30ad6908
Sebastian Bitsch <sebastianbitsch@protonmail.com>
2022-07-14 15:21:18 +0200

    added sample boards

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 from MarchingSquares import MarchingSquares, random_grid
 
+# Sample boards
+from sample_boards import sample_grids
 
 if __name__ == "__main__":
 
-    N = [5, 10, 50, 100, 150]
+    N = [5, 10, 50, 100]
 
+    # show sample grids
+    for grid in sample_grids:
+        ms = MarchingSquares(grid)
+
+        ms.plot_grid()
+        ms.plot_edges(show_grid=True)
+        ms.plot_polygons(show_grid=True)
+        ms.plot_polygons(show_grid=False)    
+
+    # show random grids
     for n in N:
         grid = random_grid((n,n))
-
         ms = MarchingSquares(grid)
 
         ms.plot_grid()
         ms.plot_edges(show_grid=True)
         ms.plot_polygons(show_grid=True)
         ms.plot_polygons(show_grid=False)
+
